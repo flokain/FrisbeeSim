3187a64f99b713d3bf89ec3a2ec72a481f02e2f4
flokain <flokain11@gmail.com>
2016-02-19 09:10:28 +0100

    added FixedEntity: holds cones, lines etc

---
+++ src/Ultimate/Ball.java
@@ -0,0 +1,50 @@
+package Ultimate;
+
+import java.io.IOException;
+
+import Ultimate.UltimateEntity.AccelerationsContainer;
+import disc.physics.aerodynamics.FlightModel_HummelNew;
+import disc.physics.aerodynamics.FlyingDisc;
+import disc.physics.aerodynamics.FlightModel_HummelNew.DeValueContainer;
+import disc.physics.aerodynamics.FlyingDisc.flightCoefficientsType;
+import sim.engine.Steppable;
+import sim.util.Double3D;
+
+@SuppressWarnings("serial")
+public class Ball extends Frisbee implements Steppable
+{
+	public Ball() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public AccelerationsContainer calcAccelerations(Ultimate ultimate)
+	{	
+		if (position.z - radius <= 0)  //touching the ground
+		{
+			velocity.multiplyIn(0.8); // reflect and take some away due to friction. 
+			velocity.z = Math.abs(velocity.z);
+			omega.multiplyIn(-0.99);
+			
+		}
+		acceleration.setTo(getVelocity().negate().resize(velocity.lengthSq()).multiply(Math.pow(radius,2)*Math.PI * 0.5).subtract(new Double3D(0,0,10))); // air friction on a ball
+		alpha.setTo(getOmega().negate().multiply(0.8));
+		return new AccelerationsContainer(new Double3D(acceleration),new Double3D(alpha));
+	}
+	
+	public void getThrown(Double3D velocity, Double3D orientation, Double3D omega)
+	{
+		this.isFlying = true;
+		this.velocity.setTo(velocity);
+		this.orientation.setTo(orientation);
+		this.omega.setTo(omega);
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isFlying()
+	{
+		return isFlying;
+	}
+}
---
+++ src/Ultimate/FixedEntity.java
@@ -0,0 +1,36 @@
+package Ultimate;
+
+import sim.util.Double3D;
+
+// Object doesn't change during simulation and therefore doesnot implement "stepable".
+// this objects are lines on the ground, cones, trees...
+public class FixedEntity {
+
+	Double3D position;
+	Double3D top;
+	
+	// Transform3d 
+	
+	public FixedEntity(Double3D position, Double3D top) 
+	{
+		this.position = position;
+		this.top = top;
+	}
+
+	public Double3D getPosition() {
+		return position;
+	}
+
+	public void setPosition(Double3D position) {
+		this.position = position;
+	}
+
+	public Double3D getTop() {
+		return top;
+	}
+
+	public void setTop(Double3D top) {
+		this.top = top;
+	}
+	
+}
